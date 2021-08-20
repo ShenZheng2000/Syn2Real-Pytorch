c1c679d459653fbaf223a07158dea45c4bab5e87
ShenZheng2000 <69662345+ShenZheng2000@users.noreply.github.com>
2021-08-20 23:00:59 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,22 +1,72 @@
 # --- Imports --- #
 import argparse
+import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
+from data.train_data import TrainData
 from data.val_data import ValData
 from modeling.model import DeRain_v2
+from modeling.GP import GPStruct
 from utils import *
+from torchvision.models import vgg16
+from modeling.perceptual import LossNetwork
 import os
 import numpy as np
 import random
+plt.switch_backend('agg')
 
 
-class Tester():
+class Trainer():
     def __init__(self, args):
+        self.learning_rate = args.learning_rate
+        self.crop_size = args.crop_size
+        self.train_batch_size = args.train_batch_size
+        self.epoch_start = args.epoch_start
         self.lambda_loss = args.lambda_loss
         self.val_batch_size = args.val_batch_size
         self.category = args.category
+        self.version = args.version  # version1 is GP model used in conference paper and version2 is GP model (feature level GP) used in journal paper
+        self.kernel_type = args.kernel_type
         self.exp_name = args.exp_name
+        self.lambgp = args.lambda_GP
+        self.use_GP_inlblphase = False  # indication whether or not to use GP during labeled phase
+
+        self.labeled_name = args.labeled_name
+        self.unlabeled_name = args.unlabeled_name
+        self.val_filename = args.val_filename
+
         self.device_ids = [Id for Id in range(torch.cuda.device_count())]
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        self.net = DeRain_v2().to(self.device)
+        self.num_epochs, self.train_data_dir, self.val_data_dir = self.get_type(self.category)
+        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=self.learning_rate)
+
+        # --- Load training data and validation/test data --- #
+        self.unlbl_train_data_loader = DataLoader(TrainData(self.crop_size, self.train_data_dir, self.unlabeled_name),
+                                                            batch_size=self.train_batch_size, shuffle=True, num_workers=0)
+        self.lbl_train_data_loader = DataLoader(TrainData(self.crop_size, self.train_data_dir, self.labeled_name),
+                                                        batch_size=self.train_batch_size,
+                                                        shuffle=True, num_workers=0)
+        self.val_data_loader = DataLoader(ValData(self.val_data_dir, self.val_filename),
+                                                    batch_size=self.val_batch_size,
+                                                    shuffle=False,
+                                                    num_workers=0)
+
+        self.num_labeled = self.train_batch_size * len(self.lbl_train_data_loader)  # number of labeled images
+        self.num_unlabeled = self.train_batch_size * len(self.unlbl_train_data_loader)  # number of unlabeled images
+
+        self.gp_struct = GPStruct(self.num_labeled, self.num_unlabeled, self.train_batch_size, self.version,
+                                  self.kernel_type)
+
+    def percep_net(self):
+        vgg_model = vgg16(pretrained=True).features[:16]
+        vgg_model = vgg_model.to(self.device)
+        # vgg_model = nn.DataParallel(vgg_model, device_ids=device_ids)
+        for param in vgg_model.parameters():
+            param.requires_grad = False
+        loss_network = LossNetwork(vgg_model)
+        loss_network.eval()
+        return loss_network
 
     def get_seed(self, seed):
         if seed:
@@ -26,62 +76,202 @@ class Tester():
             random.seed(seed)
             print('Seed:\t{}'.format(seed))
 
-    def test(self):
-        print('--- Hyper-parameters for testing ---')
-        print('val_batch_size: {}\nlambda_loss: {}\ncategory: {}'
-              .format(self.val_batch_size, self.lambda_loss, self.category))
-
-        # --- Set category-specific hyper-parameters  --- #
-        if self.category == 'derain':
+    def get_type(self, category):
+        if category == 'derain':
+            num_epochs = 200
+            train_data_dir = './data/train/derain/'
             val_data_dir = './data/test/derain/'
-        elif self.category == 'dehaze':
+        elif category == 'dehaze':
+            num_epochs = 10
+            train_data_dir = './data/train/dehaze/'
             val_data_dir = './data/test/dehaze/'
         else:
             raise Exception('Wrong image category. Set it to derain or dehaze dateset.')
 
-        # --- Validation data loader --- #
-        val_filename = args.val_filename
-        val_data_loader = DataLoader(ValData(val_data_dir, val_filename),
-                                     batch_size=self.val_batch_size,
-                                     shuffle=False,
-                                     num_workers=0)
+        return num_epochs, train_data_dir, val_data_dir
 
-        # --- Define the network --- #
-        net = DeRain_v2().to(self.device)
+    def load_train_weight(self, exp_name, net, category):
+        if os.path.exists('./{}/'.format(exp_name)) == False:
+            os.mkdir('./{}/'.format(exp_name))
+        try:
+            net.load_state_dict(torch.load('./{}/{}_best'.format(exp_name, category)))
+            print('--- weight loaded ---')
+        except:
+            print('--- no weight loaded ---')
+
+    def cal_params(self, net):
+        pytorch_total_params = sum(p.numel() for p in net.parameters() if p.requires_grad)
+        print("Total_params: {}".format(pytorch_total_params))
+
+    def print_params(self):
+        print('--- Hyper-parameters for training ---')
+        print(
+            'learning_rate: {}\ncrop_size: {}\ntrain_batch_size: {}\nval_batch_size: {}\nlambda_loss: {}\ncategory: {}'.format(
+                self.learning_rate, self.crop_size,
+                self.train_batch_size, self.val_batch_size, self.lambda_loss, self.category))
+
+    def train_label(self, epoch):
+        if self.lambgp != 0 and self.use_GP_inlblphase == True:
+            self.gp_struct.gen_featmaps(self.lbl_train_data_loader, self.net, self.device)
+
+        for batch_id, train_data in enumerate(self.lbl_train_data_loader):
+
+            input_image, gt, imgid = train_data
+            input_image = input_image.to(self.device)
+            gt = gt.to(self.device)
+
+            # --- Zero the parameter gradients --- #
+            self.optimizer.zero_grad()
+
+            # --- Forward + Backward + Optimize --- #
+            self.net.train()
+            pred_image, zy_in = self.net(input_image)
+
+            smooth_loss = F.smooth_l1_loss(pred_image, gt)
+            perceptual_loss = self.loss_network(pred_image, gt)
+            gp_loss = 0
+            if self.lambgp != 0 and self.use_GP_inlblphase == True:
+                gp_loss = self.gp_struct.compute_gploss(zy_in, imgid, batch_id, 1)
+            loss = smooth_loss + self.lambda_loss * perceptual_loss + self.lambgp * gp_loss
+
+            loss.backward()
+            self.optimizer.step()
+
+            # --- To calculate average PSNR --- #
+            self.psnr_list.extend(to_psnr(pred_image, gt))
+
+            if not (batch_id % 100):
+                print('Epoch: {0}, Iteration: {1}'.format(epoch, batch_id))
+
+        # --- Calculate the average training PSNR in one epoch --- #
+        train_psnr = sum(self.psnr_list) / len(self.psnr_list)
+
+        # --- Save the network parameters --- #
+        torch.save(self.net.state_dict(), './{}/{}'.format(self.exp_name, self.category))
+
+        self.evaluate(epoch=epoch, train_psnr=train_psnr)
+
+        if self.lambgp != 0:
+            self.gp_struct.gen_featmaps(self.lbl_train_data_loader, self.net, self.device)
+
+    def train_unlabel(self, epoch):
+        if self.lambgp != 0:
+            self.gp_struct.gen_featmaps_unlbl(self.unlbl_train_data_loader, self.net, self.device)
+
+            for batch_id, train_data in enumerate(self.unlbl_train_data_loader):
+
+                input_image, gt, imgid = train_data
+                input_image = input_image.to(self.device)
+                gt = gt.to(self.device)
+
+                # --- Zero the parameter gradients --- #
+                self.optimizer.zero_grad()
+
+                # --- Forward + Backward + Optimize --- #
+                self.net.train()
+                pred_image, zy_in = self.net(input_image)
+                gp_loss = 0
+
+                if self.lambgp != 0:
+                    gp_loss = self.gp_struct.compute_gploss(zy_in, imgid, batch_id, 0)
+
+                loss = self.lambgp * gp_loss
+                if loss != 0:
+                    loss.backward()
+                    self.optimizer.step()
+
+                # --- To calculate average PSNR --- #
+                self.psnr_list.extend(to_psnr(pred_image, gt))
+
+                if not (batch_id % 100):
+                    print('Epoch: {0}, Iteration: {1}'.format(epoch, batch_id))
+
+            # --- Calculate the average training PSNR in one epoch --- #
+            train_psnr = sum(self.psnr_list) / len(self.psnr_list)
+
+            # --- Save the network parameters --- #
+            torch.save(self.net.state_dict(), './{}/{}'.format(self.exp_name, self.category))
+
+            self.evaluate(epoch=epoch, train_psnr=train_psnr)
+
+    def evaluate(self, epoch, train_psnr):
+        # --- Use the evaluation model in testing --- #
+        self.net.eval()
+
+        val_psnr, val_ssim = validation(self.net, self.val_data_loader, self.device, self.category, self.exp_name)
+        one_epoch_time = time.time() - self.start_time
+        print_log(epoch + 1, self.num_epochs, one_epoch_time, train_psnr, val_psnr, val_ssim, self.category, self.exp_name)
+
+        # --- update the network weight --- #
+        if val_psnr >= self.old_val_psnr:
+            torch.save(self.net.state_dict(), './{}/{}_best'.format(self.exp_name, self.category))
+            print('model saved')
+            self.old_val_psnr = val_psnr
+
+
+    def train(self):
+
+        # set seed
+        self.get_seed(args.seed)
+
+        # print hyperparameters
+        self.print_params()
+
+        # --- Define the perceptual loss network --- #
+        self.loss_network = self.percep_net()
 
         # --- Load the network weight --- #
-        net.load_state_dict(torch.load('./{}/{}_best'.format(self.exp_name, self.category)))
+        self.load_train_weight(self.exp_name, self.net, self.category)
 
-        # --- Use the evaluation model in testing --- #
-        net.eval()
-        if os.path.exists('./{}_results/{}/'.format(self.category, self.exp_name)) == False:
-            os.mkdir('./{}_results/{}/'.format(self.category, self.exp_name))
-            os.mkdir('./{}_results/{}/rain/'.format(self.category, self.exp_name))
+        # --- Calculate all trainable parameters in network --- #
+        self.cal_params(self.net)
 
-        print('--- Testing starts! ---')
-        start_time = time.time()
-        val_psnr, val_ssim = validation(net, val_data_loader, self.device, self.category, self.exp_name, save_tag=True)
-        end_time = time.time() - start_time
-        print('val_psnr: {0:.2f}, val_ssim: {1:.4f}'.format(val_psnr, val_ssim))
-        print('validation time is {0:.4f}'.format(end_time))
+
+        # --- Previous PSNR and SSIM in testing --- #
+        self.net.eval()
+        self.old_val_psnr, self.old_val_ssim = validation(self.net, self.val_data_loader, self.device, self.category, self.exp_name)
+        print('old_val_psnr: {0:.2f}, old_val_ssim: {1:.4f}'.format(self.old_val_psnr, self.old_val_ssim))
+
+        self.net.train()
+        for epoch in range(self.epoch_start, self.num_epochs):
+            self.psnr_list = []
+            self.start_time = time.time()
+            adjust_learning_rate(self.optimizer, epoch, category=self.category)
+
+            # -------------------------------------------------------------------------------------------------------------
+            # Labeled phase
+            self.train_label(epoch)
+
+            # -------------------------------------------------------------------------------------------------------------
+            # Unlabeled Phase
+            self.train_unlabel(epoch)
+
+
 
 
 
 if __name__ == "__main__":
-
     # --- Parse hyper-parameters  --- #
-    parser = argparse.ArgumentParser(description='Hyper-parameters for GridDehazeNet')
-    parser.add_argument('-lambda_loss', default=0.04, type=float, help='Set the lambda in loss function',)
+    parser = argparse.ArgumentParser(description='Hyper-parameters for network')
+    parser.add_argument('-learning_rate', default=2e-4, type=float, help='Set the learning rate', )
+    parser.add_argument('-crop_size', default=[256, 256], nargs='+', type=int, help='Set the crop_size')
+    parser.add_argument('-train_batch_size', default=18, type=int, help='Set the training batch size')
+    parser.add_argument('-epoch_start', default=0, type=int, help='Starting epoch number of the training')
+    parser.add_argument('-lambda_loss', default=0.04, type=float, help='Set the lambda in loss function')
     parser.add_argument('-val_batch_size', default=1, type=int, help='Set the validation/test batch size')
-    parser.add_argument('-exp_name', type=str, help='directory for saving the networks of the experiment')
     parser.add_argument('-category', default='derain', type=str, help='Set image category (derain or dehaze?)')
-    parser.add_argument('-val_filename', default='SIRR_test.txt', type=str, help='dataset for testing (real_input_split1.txt or SIRR_test.txt)',)
-    parser.add_argument('-seed', help='set random seed', default=19, type=int)
+    parser.add_argument('-version', default='version1', type=str, help='Set the GP model (version1 or version2?)')
+    parser.add_argument('-kernel_type', default='Linear', type=str, help='Set the GP model (Linear or Squared_exponential or Rational_quadratic?)')
+    parser.add_argument('-exp_name', type=str, help='directory for saving the networks of the experiment')
+    parser.add_argument('-lambda_GP', default=0.0015, type=float, help='Set the lambda_GP for gploss in loss function')
+    parser.add_argument('-seed', default=19, type=int, help='set random seed')
+
+    parser.add_argument('-labeled_name', default='real_input_split1.txt', type=str)
+    parser.add_argument('-unlabeled_name', default='real_input_split1.txt', type=str)
+    parser.add_argument('-val_filename', default='SIRR_test.txt', type=str)
+
     args = parser.parse_args()
 
-    t = Tester(args)
-    t.test()
-
-
-
+    t = Trainer(args)
+    t.train()
 
